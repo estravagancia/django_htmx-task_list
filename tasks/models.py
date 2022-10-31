6f813d65eefb1169e7851b4c92c0bb7f1ab15afb
estravagancia <webmaster@estravagancia.com>
2022-10-31 12:57:47 +0000

    Añadiendo fecha

--- tasks/models.py
+++ tasks/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     description=models.CharField(max_length=75)
+    created_at=models.DateTimeField(auto_now=True)
